39965c6d72b0ffe5ed498ac9e1bf2b1958924c7b
DaylenJones <jonesday001@warren.k12.in.us>
2025-05-13 21:52:53 -0400

    ud

--- src/objects/Cannon.java
+++ src/objects/Cannon.java
@@ -1,23 +1,25 @@
 package objects;
 
+import main.Game;
+
 public class Cannon extends GameObject {
 
     private int tileY;
 
     public Cannon(int x, int y, int objType) {
         super(x, y, objType);
-        // TODO: set tileY to y / Game.TILES_SIZE
-        // TODO: call initHitbox() passing in 40, 26
-        // TODO: subtract (int)(4 * Game.SCALE) from hitbox.x
-        // TODO: add (int)(6 * Game.SCALE) to hitbox.y
+        tileY = y / Game.TILES_SIZE;
+        initHitbox(40, 26);
+        hitbox.x -= (int)(4 * Game.SCALE);
+        hitbox.y += (int)(6 * Game.SCALE);
     }
 
-    public void update(){
-        // TODO: check if doAnimation is true and if so updateAnimationTick()
+    public void update() {
+        if (doAnimation)
+            updateAnimationTick();
     }
 
     public int getTileY() {
-        // TODO: return tileY
-        return 0; // TODO: remove when done
+        return tileY;
     }
 }
--- src/objects/Spike.java
+++ src/objects/Spike.java
@@ -1,12 +1,14 @@
 package objects;
 
-public class Spike extends GameObject{
+import static utilz.Game.SCALE;
+
+public class Spike extends GameObject {
     public Spike(int x, int y, int objType) {
         super(x, y, objType);
 
-        // TODO: call initHitbox passing in 32, and 16
-        // TODO: set xDrawOffset to 0
-        // TODO: set yDrawOffset to (int) (Game.SCALE * 16)
-        // TODO: add yDrawOffset to hitbox.y
+        initHitbox(32, 16);
+        xDrawOffset = 0;
+        yDrawOffset = (int) (SCALE * 16);
+        hitbox.y += yDrawOffset;
     }
 }
